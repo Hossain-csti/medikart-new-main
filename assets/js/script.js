6aaf617300396f6928cc699552485d11644eb4a7
Hossain Ahmed <hossainahmed.7709@gmail.com>
2023-06-20 02:57:22 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $one = $("#one");
+    $two = $("#two");
+    $one.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+    $two.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
